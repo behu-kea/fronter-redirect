7e9c9f953f2cf2e2250d413e3cdd6567472ff937
Benjamin Hughes <behu@kea.dk>
2022-08-05 13:46:16 +0200

    Fixed small bug

--- fronter-redirect.js
+++ fronter-redirect.js
@@ -2,17 +2,12 @@ const iframe = document.querySelector("section main iframe");
 
 document
   .querySelector("ul.l-main-content-nav")
-  .addEventListener("click", () => {
-    const activeLink = document.querySelector(
-      "ul.l-main-content-nav > li > .active"
-    );
-
+  .addEventListener("click", (clickEvent) =>
     browser.storage.sync.set({
-      activeLink: activeLink.getAttribute("href"),
-    });
-  });
+      activeLink: clickEvent.target.getAttribute("href"),
+    })
+  );
 
-browser.storage.sync.get("activeLink").then(({ activeLink }) => {
-  console.log(activeLink);
-  iframe.setAttribute("src", activeLink);
-});
+browser.storage.sync
+  .get("activeLink")
+  .then(({ activeLink }) => iframe.setAttribute("src", activeLink));
