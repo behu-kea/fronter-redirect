d4a41ea6fa3d98c8d612c2608cb0cd89198df53f
Benjamin Hughes <behu@kea.dk>
2022-08-08 13:16:16 +0200

    Chrome version working

--- chrome/fronter-redirect.js
+++ chrome/fronter-redirect.js
@@ -3,11 +3,11 @@ const iframe = document.querySelector("section main iframe");
 document
   .querySelector("ul.l-main-content-nav")
   .addEventListener("click", (clickEvent) =>
-    browser.storage.sync.set({
+    chrome.storage.sync.set({
       activeLink: clickEvent.target.getAttribute("href"),
     })
   );
 
-browser.storage.sync
+chrome.storage.sync
   .get("activeLink")
   .then(({ activeLink }) => iframe.setAttribute("src", activeLink));
