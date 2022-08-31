c3f4eb4c8c2c01581d40991fa5099671e2462a75
Benjamin Hughes <behu@kea.dk>
2022-08-31 15:58:49 +0200

    asd

--- firefox/fronter-redirect.js
+++ firefox/fronter-redirect.js
@@ -1,13 +1,35 @@
+console.log("Script initialised");
+
 const iframe = document.querySelector("section main iframe");
+// For fronterroom save the latest visited link
+const actionUrl = document
+    .querySelector("#aspnetForm").getAttribute("action");
+const fronterRoomId = actionUrl.split("./ContentArea.aspx?LocationID=")[1].split("&LocationType")[0];
 
 document
-  .querySelector("ul.l-main-content-nav")
-  .addEventListener("click", (clickEvent) =>
-    browser.storage.sync.set({
-      activeLink: clickEvent.target.getAttribute("href"),
-    })
-  );
+    .querySelector("ul.l-main-content-nav")
+    .addEventListener("click", (clickEvent) => {
+        const clickedHref = clickEvent.target.getAttribute("href");
+
+        browser.storage.sync
+            .get(fronterRoomId)
+            .then((lastClickedLink) => {
+                lastClickedLink[fronterRoomId] = clickedHref;
+
+                browser.storage.sync.set(lastClickedLink);
+            });
+    });
+
+//browser.storage.sync.remove('fronterRoomLinks');
 
 browser.storage.sync
-  .get("activeLink")
-  .then(({ activeLink }) => iframe.setAttribute("src", activeLink));
+    .get(fronterRoomId)
+    .then(lastClickedLink => {
+        if (lastClickedLink) {
+            iframe.setAttribute("src", lastClickedLink[fronterRoomId]);
+        }
+    });
+
+
+
+
