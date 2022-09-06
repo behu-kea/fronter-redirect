ef5efb5ad3587d361a7329c2135d5fd6c96787dd
Benjamin Hughes <behu@kea.dk>
2022-09-06 09:59:41 +0200

    fixed problems between firefox and chrome

--- chrome/fronter-redirect.js
+++ chrome/fronter-redirect.js
@@ -1,13 +1,56 @@
+console.log("Script initialised");
 const iframe = document.querySelector("section main iframe");
 
+// For fronterroom save the latest visited link
+const actionUrl = document
+    .querySelector("#aspnetForm").getAttribute("action");
+const fronterRoomId = getLocationId(actionUrl);
+
 document
-  .querySelector("ul.l-main-content-nav")
-  .addEventListener("click", (clickEvent) =>
-    chrome.storage.sync.set({
-      activeLink: clickEvent.target.getAttribute("href"),
-    })
-  );
+    .querySelector("ul.l-main-content-nav")
+    .addEventListener("click", (clickEvent) => {
+        console.log('Link clicked');
+        const clickedHref = clickEvent.target.getAttribute("href");
+        chrome.storage.sync
+            .get(fronterRoomId)
+            .then((lastClickedLink) => {
+                lastClickedLink[fronterRoomId] = clickedHref;
+
+                chrome.storage.sync.set(lastClickedLink);
+            });
+    });
+
+//chrome.storage.sync.remove('fronterRoomLinks');
 
 chrome.storage.sync
-  .get("activeLink")
-  .then(({ activeLink }) => iframe.setAttribute("src", activeLink));
+    .get(fronterRoomId)
+    .then(lastClickedLink => {
+        // firefox returns undefined chrome an empty object
+        if (lastClickedLink && Object.keys(lastClickedLink).length !== 0) {
+            iframe.setAttribute("src", lastClickedLink[fronterRoomId]);
+        }
+    });
+
+
+function getLocationId(actionString) {
+    actionString = actionString.toLowerCase();
+    const regex = /locationid=(\d*)/gm;
+
+    let matchedLocationId;
+    let m;
+
+    while ((m = regex.exec(actionString)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+
+        // The result can be accessed through the `m`-variable.
+        m.forEach((match, groupIndex) => {
+            matchedLocationId = match;
+        });
+    }
+
+    return matchedLocationId;
+}
+
--- firefox/fronter-redirect.js
+++ firefox/fronter-redirect.js
@@ -1,16 +1,16 @@
 console.log("Script initialised");
-
 const iframe = document.querySelector("section main iframe");
+
 // For fronterroom save the latest visited link
 const actionUrl = document
     .querySelector("#aspnetForm").getAttribute("action");
-const fronterRoomId = actionUrl.split("./ContentArea.aspx?LocationID=")[1].split("&LocationType")[0];
+const fronterRoomId = getLocationId(actionUrl);
 
 document
     .querySelector("ul.l-main-content-nav")
     .addEventListener("click", (clickEvent) => {
+        console.log('Link clicked');
         const clickedHref = clickEvent.target.getAttribute("href");
-
         browser.storage.sync
             .get(fronterRoomId)
             .then((lastClickedLink) => {
@@ -25,11 +25,32 @@ document
 browser.storage.sync
     .get(fronterRoomId)
     .then(lastClickedLink => {
-        if (lastClickedLink) {
+        // firefox returns undefined chrome an empty object
+        if (lastClickedLink && Object.keys(lastClickedLink).length !== 0) {
             iframe.setAttribute("src", lastClickedLink[fronterRoomId]);
         }
     });
 
 
+function getLocationId(actionString) {
+    actionString = actionString.toLowerCase();
+    const regex = /locationid=(\d*)/gm;
 
+    let matchedLocationId;
+    let m;
+
+    while ((m = regex.exec(actionString)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+
+        // The result can be accessed through the `m`-variable.
+        m.forEach((match, groupIndex) => {
+            matchedLocationId = match;
+        });
+    }
+
+    return matchedLocationId;
+}
 
